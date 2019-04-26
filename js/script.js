47fa63d6351895b5832f682648314e6f40a84657
muhindokiro <blaize1143@gmail.com>
2019-04-26 15:57:49 +0300

    add equilateral formula

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-function space(){
-  var sidea = parseInt(document.getElementById('fvalue').value);
-  var sideb = parseInt(document.getElementById('svalue').value);
-  var sidec = parseInt(document.getElementById('tvalue').value);
+function display(){
+  var Sa = parseInt(document.getElementById("fvalue").value);
+  var Sb = parseInt(document.getElementById("svalue").value);
+  var Sc = parseInt(document.getElementById("tvalue").value);
+
+  if (Sa+Sb<=Sc || Sa+Sc<=Sb || Sb+Sc<=Sa){
+  document.getElementById("result").innerHTML="The values do NOT make a triangle.";
   }
+    else if (Sa === Sb && Sa === Sc && Sb === Sc) {
+        document.getElementById("result").innerHTML="The values make an Equilateral triangle.";
+    }
+    else if (Sa !== Sb && Sa !== Sc && Sb !== Sc) {
+    document.getElementById("result").innerHTML="The values make a Scalene triangle.";
+    }
+    else if(Sa === Sb || Sa === Sc || Sb === Sc) {
+    document.getElementById("result").innerHTML="The values make an Isosceles triangle.";
+
+    }
+   else {
+
+}
+}
