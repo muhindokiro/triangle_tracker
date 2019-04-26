ff04ab954e006b9be1744ad084af66fe73ac6e9e
muhindokiro <blaize1143@gmail.com>
2019-04-26 17:18:46 +0300

    change background to homepage

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function display(){
   document.getElementById("result").innerHTML="The values do NOT make a triangle.";
   }
     else if (Sa === Sb && Sa === Sc && Sb === Sc) {
-        document.getElementById("result").innerHTML="The values make an Equilateral triangle.";
+    document.getElementById("result").innerHTML="The values make an Equilateral triangle.";
     }
     else if (Sa !== Sb && Sa !== Sc && Sb !== Sc) {
     document.getElementById("result").innerHTML="The values make a Scalene triangle.";
