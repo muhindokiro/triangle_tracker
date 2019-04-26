669302a26159a6e360b1659b79cbf15ae0bcbeec
muhindokiro <blaize1143@gmail.com>
2019-04-26 14:40:08 +0300

    add variables to function

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function space(){
+  var sidea = parseInt(document.getElementById('fvalue').value);
+  var sideb = parseInt(document.getElementById('svalue').value);
+  var sidec = parseInt(document.getElementById('tvalue').value);
+  }
