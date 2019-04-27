95fe32dfdaa1bf5b68e3bbf1a1a1a517e56dae57
muhindokiro <blaize1143@gmail.com>
2019-04-27 17:15:46 +0300

    add styling to final paragragh

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 function display(){
-  var Sa = parseInt(document.getElementById("fvalue").value);
-  var Sb = parseInt(document.getElementById("svalue").value);
-  var Sc = parseInt(document.getElementById("tvalue").value);
+  var Sa = parseInt(document.getElementById("fno.").value);
+  var Sb = parseInt(document.getElementById("sno.").value);
+  var Sc = parseInt(document.getElementById("tno.").value);
 
   if (Sa+Sb<=Sc || Sa+Sc<=Sb || Sb+Sc<=Sa){
-  document.getElementById("result").innerHTML="The values do NOT make a triangle."
+  document.getElementById("result").innerHTML="Do NOT Form a triangle."
   }
     else if (Sa === Sb && Sa === Sc && Sb === Sc) {
-    document.getElementById("result").innerHTML="The values make an Equilateral triangle.";
+    document.getElementById("result").innerHTML="Form an Equilateral triangle.";
     }
     else if (Sa !== Sb && Sa !== Sc && Sb !== Sc) {
-    document.getElementById("result").innerHTML="The values make a Scalene triangle.";
+    document.getElementById("result").innerHTML="Form a Scalene triangle.";
     }
     else if(Sa === Sb || Sa === Sc || Sb === Sc) {
-    document.getElementById("result").innerHTML="The values make an Isosceles triangle.";
+    document.getElementById("result").innerHTML="Form an Isosceles triangle.";
 
     }
    else {
