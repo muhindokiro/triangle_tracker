4dc8c8f765ff0c6ca22c8027399b750f1f75df88
muhindokiro <blaize1143@gmail.com>
2019-04-27 16:52:13 +0300

    change text alignment in the border

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function display(){
   var Sc = parseInt(document.getElementById("tvalue").value);
 
   if (Sa+Sb<=Sc || Sa+Sc<=Sb || Sb+Sc<=Sa){
-  document.getElementById("result").innerHTML="The values do NOT make a triangle.";
+  document.getElementById("result").innerHTML="The values do NOT make a triangle."
   }
     else if (Sa === Sb && Sa === Sc && Sb === Sc) {
     document.getElementById("result").innerHTML="The values make an Equilateral triangle.";
